1155cb09fd6f45c5cfbd42880ffed1f16d57aaba
Suzuki Taro <4222502+hikaen2@users.noreply.github.com>
2020-11-01 00:15:40 +0900

    create from https://search.maven.org/artifact/org.apache.struts/struts2-archetype-convention/2.5.22/maven-archetype

---
+++ src/test/java/com/example/actions/HelloActionTest.java
@@ -0,0 +1,18 @@
+package com.example.actions;
+
+import org.apache.struts2.StrutsTestCase;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloActionTest extends StrutsTestCase {
+
+    public void testHelloAction() throws Exception {
+        HelloAction hello = new HelloAction();
+        ActionContext.getContext().getContainer().inject(hello);
+        String result = hello.execute();
+        assertEquals("Expected a success result!", ActionSupport.SUCCESS, result);
+        assertEquals("Expected the default message!", hello.getText(HelloAction.MESSAGE), hello.getMessage());
+    }
+
+}
---
+++ src/test/java/com/example/actions/IndexTest.java
@@ -0,0 +1,15 @@
+package com.example.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.StrutsTestCase;
+
+public class IndexTest extends StrutsTestCase {
+
+    public void testIndex() {
+        Index index = new Index();
+        String result = index.execute();
+        assertEquals("Expected a success result!", ActionSupport.SUCCESS, result);
+        assertEquals("Expected the 'hello' action name!!", "hello", index.getRedirectName());
+    }
+
+}
